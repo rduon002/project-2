704297f5221a5ab6a3975d6e854f74891317a2c7
Nigel <nigellevine@outlook.com>
2018-08-23 18:59:35 -0700

    server ready

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// require("dotenv").config();
+require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 
 // var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
