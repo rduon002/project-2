d020809571651ab5cf6535a5b26f5228525f0141
Nigel <nigellevine@outlook.com>
2018-08-23 19:32:53 -0700

    adding stuff

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,19 @@
-var db = require("../models");
+let db = require("../models");
+
+let path = require("path");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "/../views/index.html"));
+});
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+app.get("/search", function(req, res) {
+  res.sendFile(path.join(__dirname, "/../views/search.html"));
+});
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+// If no matching route is found default to home
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/../views/index.html"));
+});
 };
